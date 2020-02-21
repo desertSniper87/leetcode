5c5c1aea95244e8efa38549be287234e5570f1f0
desertsniper87 <torshobuet@gmail.com>
2020-02-21 22:44:20 +0600

    new go code

--- go/rain_water/main.go
+++ go/rain_water/main.go
@@ -9,22 +9,44 @@ func max(a, b int) int {
 	return b
 }
 
-func main() {
-	height := []int{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}
-	fmt.Println(trap(height))
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
 
+
 func trap(height []int) int {
-	length := len(height)
+    if len(height) == 0 {
+        return 0
+    }
+	var length = len(height)
 
-	var left_max, right_max [length]int
+    var left_max = make([] int, length)
+    var right_max = make([] int, length)
 
-	//left_max[0] = height[0]
-	//fmt.Println(max(1, 2))
+    left_max[0] = height[0]
+    right_max[length-1] = height[length-1]
 
-	//for i := 1; i < length; i++ {
-	////left_max[i] = max(left_max[i], height[i])
-	//}
+    for i := 1; i < length; i++ {
+        left_max[i] = max(height[i], left_max[i-1])
+    }
 
-	return length
+    for i := length-2; i >= 0; i-- {
+        right_max[i] = max(height[i], right_max[i+1])
+    }
+
+    var ans = 0
+
+    for i := 0; i < length; i++ {
+        ans += min(left_max[i], right_max[i]) - height[i]
+    }
+    return ans
+
+}
+
+func main() {
+    height := []int{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}
+    fmt.Println(trap(height))
 }
