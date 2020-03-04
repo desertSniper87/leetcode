15ae644f5156251297d0acbb68745a311471e002
desertsniper87 <torshobuet@gmail.com>
2020-03-04 19:36:39 +0600

    reset code

--- go/207_course_schedule/course_schedule.go
+++ go/207_course_schedule/course_schedule.go
@@ -2,86 +2,51 @@ package course_schedule
 
 import "fmt"
 
-// Graph : represents a Graph
-//type Graph struct {
-    //nodes []*GraphNode
-//}
+func getIncomingEdges(graph map[int][]int, vertex int) []int {
+	v_list := []int{}
+	for i, v := range graph {
+		//fmt.Println("i: ", i, "v: ", v)
+		for _, out_vert := range v {
+			//fmt.Println("out_vert", out_vert, "vertex", vertex)
+			if vertex == out_vert {
+				v_list = append(v_list, i)
+			}
+		}
+	}
 
- ////GraphNode : represents a Graph node
-//type GraphNode struct {
-    //id    int
-    //edges map[int]int
-//}
-
-//func newNode (id int) *GraphNode {
-    //return &GraphNode{
-        //id: id,
-        //edges: make(map[int]int),
-    //}
-//}
-
-//func NewGraph() *Graph {
-    //return &Graph{
-        //nodes: []*GraphNode{},
-    //}
-//}
-
-//func (g *Graph) AddNode(id int) () {
-    //id = len(g.nodes)
-    //g.nodes = append(g.nodes, &GraphNode{
-        //id:    id,
-        //edges: make(map[int]int),
-    //})
-    //return
-//}
-
-//func (g *Graph) GetNodeFromValue(val int) (node *GraphNode, is_found bool) {
-    //for _, element := range g.nodes {
-        //if element.id == val{
-            //return element, true
-        //}
-    //}
-    //return node, false
-//}
-
-//func (g* Graph) printGraph() {
-    //for _, element := range g.nodes {
-        //fmt.Println(element.id, element.edges)
-    //}
-    //return
-//}
-
-func toposort (adjList map[int][]int, visited map[int]bool, src int) map[int]bool {
-    visited[src] = true
-    for _, i := range adjList[src] {
-        if visited[i] == false{
-            visited = toposort(adjList, visited, i)
-        }
-    }
-    return visited
+	//fmt.Println(v_list)
+	return v_list
 }
 
 func canFinish(numCourses int, prerequisites [][]int) bool {
-    var adjList = make(map[int][]int, len(prerequisites))
-    var visited = make(map[int]bool, 0)
+	var adjList = make(map[int][]int, len(prerequisites))
+	var visited = make(map[int]bool, 0)
 
-    fmt.Println(adjList[1])
-    for _, element := range prerequisites {
-        src := element[1]
-        dest := element[0]
+	for _, element := range prerequisites {
+		src := element[1]
+		dest := element[0]
 
-        if len(adjList[src]) == 0{
-            adjList[src] = []int{dest}
-        } else {
-            adjList[src] = append(adjList[src], dest)
-        }
+		if len(adjList[src]) == 0 {
+			adjList[src] = []int{dest}
+		} else {
+			adjList[src] = append(adjList[src], dest)
+		}
 
-        visited[src] = false
-        visited[dest] = false
-    }
+		visited[src] = false
+		visited[dest] = false
+	}
 
-    visited = toposort(adjList, visited, 0)
+	fmt.Println("adjList: ", adjList)
+	fmt.Println("visited: ", visited)
 
-    fmt.Println(visited)
-    return false
+	for vertex_n, _ := range adjList {
+		visited[vertex_n] = true
+		for _, vertex_m := range adjList[vertex_n] {
+			fmt.Println(adjList[vertex_n])
+			v_list := getIncomingEdges(adjList, vertex_m)
+			fmt.Println(vertex_m, v_list)
+		}
+	}
+
+	return true
 }
--- go/207_course_schedule/course_schedule_test.go
+++ go/207_course_schedule/course_schedule_test.go
@@ -1,22 +1,52 @@
 package course_schedule
 
 import (
-    "testing"
+	"testing"
 )
 
 /*
  *func TestCanFinish_1(t *testing.T) {
- *    input := [][]int {{1, 0}}
+ *    input := [][]int{{1, 0}}
  *    result := canFinish(2, input)
  *    if result != true {
- *       t.Errorf("incorrect, got: %v, want: %v.", result, true)
+ *        t.Errorf("incorrect, got: %v, want: %v.", result, true)
+ *    }
+ *}
+ *
+ */
+/*
+ *func TestCanFinish_2(t *testing.T) {
+ *    input := [][]int{{1, 0}, {0, 1}}
+ *    result := canFinish(2, input)
+ *    want := false
+ *    if result != want {
+ *        t.Errorf("incorrect, got: %v, want: %v.", result, want)
+ *    }
+ *}
+ *func TestCanFinish_3(t *testing.T) {
+ *    input := [][]int{{1, 0}, {0, 1}}
+ *    result := canFinish(2, input)
+ *    if result != true {
+ *        t.Errorf("incorrect, got: %v, want: %v.", result, true)
  *    }
  *}
  */
-func TestCanFinish_2(t *testing.T) {
-    input := [][]int {{1, 0}, {0, 1}}
-    result := canFinish(2, input)
-    if result != true {
-       t.Errorf("incorrect, got: %v, want: %v.", result, true)
-    }
+/*
+ *func TestIncomingEdges(t *testing.T) {
+ *    graph := [][]int{{1, 0}, {2, 1}, {3, 1}, {3, 2}}
+ *    vertex := 3
+ *    result := getIncomingEdges(graph, vertex)
+ *    want := [2]int{1, 2}
+ *    if result != want {
+ *        t.Errorf("incorrect, got: %v, want: %v.", result, want)
+ *    }
+ *}
+ */
+func TestCanFinish_4(t *testing.T) {
+	input := [][]int{{1, 0}, {2, 1}, {3, 1}, {3, 2}}
+	result := canFinish(4, input)
+	want := true
+	if result != want {
+		t.Errorf("incorrect, got: %v, want: %v.", result, want)
+	}
 }
