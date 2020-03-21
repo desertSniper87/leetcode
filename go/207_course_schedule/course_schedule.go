58e12a73b6de22987b1f00488055862d8f3d8616
desertsniper87 <torshobuet@gmail.com>
2020-03-21 19:47:27 +0600

    finish 207

--- go/207_course_schedule/course_schedule.go
+++ go/207_course_schedule/course_schedule.go
@@ -18,6 +18,59 @@ func getIncomingEdges(graph map[int][]int, vertex int) []int {
 	return v_list
 }
 
+// This function returns true if there is a cycle
+// in directed graph, else returns false.
+func isCycle(graph map[int][]int, inDegree map[int]int, numCourses int) bool {
+	// Create an queue and enqueue all vertices with
+	// indegree 0
+
+	fmt.Println("inDegree: ", inDegree)
+	queue := make([]int, 0)
+	for i := 0; i < numCourses; i++ {
+		if inDegree[i] == 0 {
+			queue = append(queue, i)
+		}
+	}
+
+	// Initialize count of visited vertices
+	count := 0
+
+	// One by one dequeue vertices from queue and enqueue
+	// adjacents if indegree of adjacent becomes 0
+	for len(queue) != 0 {
+
+		// Extract front of queue (or perform dequeue)
+		// and add it to topological order
+		fmt.Printf("queue: %v \n", queue)
+		u := queue[0]
+		queue = queue[1:]
+
+		// Iterate through all its neighbouring nodes
+		// of dequeued node u and decrease their in-degree
+		// by 1
+		//list<int>::iterator itr;
+		//for itr = adj[u].begin(); itr != adj[u].end(); itr++
+		for _, elem := range graph[u] {
+			inDegree[elem]--
+			if inDegree[elem] == 0 {
+				queue = append(queue, elem)
+			}
+		}
+
+		// If in-degree becomes zero, add it to queue
+		count++
+	}
+
+	// Check if there was a cycle
+	fmt.Println("Count: ", count)
+	fmt.Println("numCourses: ", numCourses)
+	if count != numCourses {
+		return true
+	} else {
+		return false
+	}
+}
+
 func canFinish(numCourses int, prerequisites [][]int) bool {
 	var adjList = make(map[int][]int, len(prerequisites))
 	var visited = make(map[int]bool, 0)
@@ -39,14 +92,19 @@ func canFinish(numCourses int, prerequisites [][]int) bool {
 	fmt.Println("adjList: ", adjList)
 	fmt.Println("visited: ", visited)
 
+	var inDegree = make(map[int]int, numCourses)
+
 	for vertex_n, _ := range adjList {
 		visited[vertex_n] = true
 		for _, vertex_m := range adjList[vertex_n] {
-			fmt.Println(adjList[vertex_n])
+			//fmt.Println("adjList[vertex_n]:", adjList[vertex_n])
 			v_list := getIncomingEdges(adjList, vertex_m)
-			fmt.Println(vertex_m, v_list)
+			//fmt.Println("vertex_m, v_list", vertex_m, v_list)
+			inDegree[vertex_m] = len(v_list)
 		}
 	}
 
-	return true
+	canFinish := !(isCycle(adjList, inDegree, numCourses))
+
+	return canFinish
 }
--- go/207_course_schedule/course_schedule_test.go
+++ go/207_course_schedule/course_schedule_test.go
@@ -13,8 +13,6 @@ import (
  *    }
  *}
  *
- */
-/*
  *func TestCanFinish_2(t *testing.T) {
  *    input := [][]int{{1, 0}, {0, 1}}
  *    result := canFinish(2, input)
@@ -23,30 +21,23 @@ import (
  *        t.Errorf("incorrect, got: %v, want: %v.", result, want)
  *    }
  *}
- *func TestCanFinish_3(t *testing.T) {
- *    input := [][]int{{1, 0}, {0, 1}}
- *    result := canFinish(2, input)
- *    if result != true {
- *        t.Errorf("incorrect, got: %v, want: %v.", result, true)
- *    }
- *}
  */
+func TestCanFinish_4(t *testing.T) {
+	input := [][]int{{1, 0}, {2, 1}, {1, 3}, {3, 2}}
+	result := canFinish(4, input)
+	want := false
+	if result != want {
+		t.Errorf("incorrect, got: %v, want: %v.", result, want)
+	}
+}
+
 /*
- *func TestIncomingEdges(t *testing.T) {
- *    graph := [][]int{{1, 0}, {2, 1}, {3, 1}, {3, 2}}
- *    vertex := 3
- *    result := getIncomingEdges(graph, vertex)
- *    want := [2]int{1, 2}
+ *func TestCanFinish_5(t *testing.T) {
+ *    input := [][]int{{1, 0}, {2, 1}, {3, 1}, {3, 2}}
+ *    result := canFinish(4, input)
+ *    want := true
  *    if result != want {
  *        t.Errorf("incorrect, got: %v, want: %v.", result, want)
  *    }
  *}
  */
-func TestCanFinish_4(t *testing.T) {
-	input := [][]int{{1, 0}, {2, 1}, {3, 1}, {3, 2}}
-	result := canFinish(4, input)
-	want := true
-	if result != want {
-		t.Errorf("incorrect, got: %v, want: %v.", result, want)
-	}
-}
