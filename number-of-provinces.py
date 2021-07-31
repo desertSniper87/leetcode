8b71885cf240a80524b9b48cbe6dfa2dc5a17cf8
desertsniper87 <torshobuet@gmail.com>
2021-07-31 14:53:18 +0600

    done at last

--- number-of-provinces.py
+++ number-of-provinces.py
@@ -2,42 +2,42 @@ from typing import List
 
 
 class Solution:
+    def __init__(self):
+        self.graph = dict()
+        self.visited = dict()
+
+    def DFSUtil(self, temp, v):
+        self.visited[v] = True
+        temp.append(v)
+
+        for i in self.graph[v]:
+            if not self.visited[i]:
+                temp = self.DFSUtil(temp, i)
+        return temp
+
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
         l = len(isConnected[0])
-        graph = {i: [] for i in range(l)}
+        self.graph = {i: [] for i in range(l)}
         for rx, row in enumerate(isConnected):
             for cx, col in enumerate(row):
                 if rx != cx and col == 1:
-                    graph[rx].append(cx)
+                    self.graph[rx].append(cx)
 
-        visited = {i: False for i in range(l)}
-        stack = [0]
-        visited[0] = True
+        self.visited = {i: False for i in range(l)}
         cc = []
-
-        while stack:
-            u = stack.pop(-1)
-            visited[u] = True
-            cc.append(u)
-
-            v = graph[u]
-
-            for v_ in v:
-                if not visited[v_]:
-                    stack.append(v_)
-
-            print(cc)
-        print('=======')
+        for v in range(l):
+            if not self.visited[v]:
+                temp = []
+                cc.append(self.DFSUtil(temp, v))
+        return len(cc)
 
 
-
-
-if __name__ == '__main__':
-    s = Solution()
-    input = [
-        [[1,1,0],[1,1,0],[0,0,1]],
-        [[1,0,0],[0,1,0],[0,0,1]],
-        [[1,0,0,1],[0,1,1,0],[0,1,1,1],[1,0,1,1]]
-    ]
-    for i in input:
-        print(s.findCircleNum(i))
+# if __name__ == '__main__':
+#     s = Solution()
+#     input = [
+#         [[1, 1, 0], [1, 1, 0], [0, 0, 1]],
+#         [[1, 0, 0], [0, 1, 0], [0, 0, 1]],
+#         [[1, 0, 0, 1], [0, 1, 1, 0], [0, 1, 1, 1], [1, 0, 1, 1]]
+#     ]
+#     for i in input:
+#         print(s.findCircleNum(i))
