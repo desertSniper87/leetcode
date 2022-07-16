fc9d7584867fd41bbf0728fcbe72a2a7da77f50f
desertsniper87 <torshobuet@gmail.com>
2022-07-16 22:59:37 +0600

    যেতে হবে বহুদূর

--- coin-change.py
+++ coin-change.py
@@ -4,47 +4,18 @@ from typing import List
 
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        print(coins)
+        _inf = amount + 1
 
-        n = len(coins)
-
-        table = [[0 for _ in range(0, amount + 1)] for _ in range(n)]
-
-        # pprint(table)
-
-        def getMinCoins(c, t):
-            min_coins = 0
-            i = len(c) - 1
-            while t > 0 and i >= 0:
-                if t >= c[i]:
-                    t -= c[i]
-                    min_coins += 1
-                else:
-                    i -= 1
-            if t == 0:
-                return min_coins
-            return None
-
-        pprint(table)
-
-        for coin_i in range(len(coins)):
-            for target in range(amount + 1):
-                m = getMinCoins(coins[:coin_i+1], target)
-                if target > 0:
-                    m = min(table[coin_i][target-1] + 1, m)
-                if coin_i > 0:
-                    m = min(table[coin_i-1][target], m)
-                table[coin_i][target] = m
-                pprint(table)
-
-        return table[n-1][amount]
+        table = [_inf for _ in range(amount + 1)]
+        table[0] = 0
 
 
-        # for coin_x, coin in enumerate(coins):
-        #     for i in range():
-        #         table[coin_x][coin] +=
+        for target in range(1, amount + 1):
+            for coin in coins:
+                if target - coin >= 0:
+                    table[target] = min(table[target], 1 + table[target - coin])
 
-        # for i in range(n):
+        return table[amount] if table[amount] != _inf else -1
 
 
 
@@ -52,6 +23,7 @@ class Solution:
 
 if __name__ == '__main__':
     s = Solution();
-    # print(s.coinChange([1, 2, 5], 11))
-    # print(s.coinChange([2], 3))
+    print(s.coinChange([1, 2, 5], 11))
+    print(s.coinChange([1, 10], 11))
+    print(s.coinChange([2], 3))
     print(s.coinChange([2], 4))
