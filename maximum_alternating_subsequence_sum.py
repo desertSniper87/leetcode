4a2f96e7772a2a4003821663705bb72089a2c361
desertsniper87 <torshobuet@gmail.com>
2022-12-17 22:45:46 +0600

    working solution

--- maximum_alternating_subsequence_sum.py
+++ maximum_alternating_subsequence_sum.py
@@ -1,29 +1,38 @@
 from typing import List
 
+"""
+:problem: https://leetcode.com/problems/word-subsets/
+"""
 
 class Solution:
     def maxAlternatingSum(self, nums: List[int]) -> int:
         dp = {}
 
         def getSum(index: int, even: bool) -> int:
+            print('a', index, even)
             if dp.get((index, even)) is not None:
                 return dp[(index, even)]
 
             if index == len(nums) - 1:
-                return nums[index]
+                print('b', (-nums[index], nums[index])[even])
+                return (-nums[index], nums[index])[even]
 
             if even:
+                print('c')
                 evenSum = getSum(index + 1, not even)
+                print('e', evenSum)
                 dp[(index, even)] = max(nums[index] + evenSum, evenSum)
             else:
-                oddSum = getSum(index + 1, even)
+                print('d')
+                oddSum = getSum(index + 1, not even)
+                print('f', oddSum)
                 dp[(index, even)] = max(-nums[index] + oddSum, oddSum)
 
             return dp[(index, even)]
 
         for i in range(len(nums)):
-            even = not (i % 2)
-            dp[(i, even)] = getSum(i, even)
+            # even = not (i % 2)
+            dp[(i, True)] = getSum(i, True)
 
         print(dp)
 
