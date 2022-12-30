0f794f527ec6cea236908108ae89062772f97787
desertsniper87 <torshobuet@gmail.com>
2022-12-30 19:40:41 +0600

    working solution

--- word_break.py
+++ word_break.py
@@ -1,50 +1,25 @@
 from typing import List, Set
 
 class Solution:
-    wordDictLenSet : Set[int]
-    s              : str
-    wordDict       : List[str]
-
-    def helper(self, i: int):
-        print(f"i -> {i}")
-
-        for j in self.wordDictLenSet:
-
-            if i + j > len(self.s):
-                break
-
-            print(f"j -> {j}")
-            words = filter(lambda x: len(x) == j, self.wordDict)
-            for w in words:
-                print(f"{self.s[i:i+j]} != {w}")
-                if self.s[i:i+j] == w:
-                    if i + j == len(self.s):
-                        return True
-                    return self.helper(i+j)
-
-
-
-
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        self.wordDictLenSet = set(map(len, wordDict))
-        self.s = s
-        self.wordDict = wordDict
+        dp = [False for _ in range(len(s) + 1)]
+        dp[len(s)] = True
 
-        for i in self.wordDictLenSet:
-            r = self.helper(i)
+        for i in range(len(s)-1, -1, -1):
+            for word in wordDict:
+                # print(dp, s[i:i+len(word)], word, i+len(word))
+                dp[i] = (s[i:i+len(word)] == word) and dp[i+len(word)]
+                if dp[i]:
+                    break
+            #  print(dp)
 
-            if r == True:
-                return r
-
-        r = self.helper(0)
-        if r:
-            return True
-        return False
+        return dp[0]
 
 
 if __name__ == "__main__":
     sol = Solution()
+    s = "leetcode"; wordDict = ["leet","code","neet"]; print(sol.wordBreak(s, wordDict))
     # s = "leetcode"; wordDict = ["leet","code"]; print(sol.wordBreak(s, wordDict))
     # s = "applepenapple"; wordDict = ["apple","pen"] ;  print(sol.wordBreak(s, wordDict))
     # s = "catsandog"; wordDict = ["cats","dog","sand","and","cat"]; print(sol.wordBreak(s, wordDict))
-    s = "a"; wordDict = ["a"]; print(sol.wordBreak(s, wordDict))
+    #  s = "a"; wordDict = ["a"]; print(sol.wordBreak(s, wordDict))
