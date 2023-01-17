b7f55318098c83fb8c8b3ed5b895fb531e931eca
desertsniper87 <torshobuet@gmail.com>
2023-01-17 22:24:08 +0600

    gugu gaga

--- flip-string-to-monotone-increasing.py
+++ flip-string-to-monotone-increasing.py
@@ -1,48 +1,19 @@
 class Solution:
     def minFlipsMonoIncr(self, s: str) -> int:
-        first_zeroes = 0
+        ans = 0
+        num = 0
+        for c in s:
+            if c == '0':
+                ans = min(num, ans + 1)
+            else:
+                num += 1
 
-        for i in range(len(s)):
-            if s[i] == '1':
-                break
-            first_zeroes += 1
-
-        s = s[first_zeroes:]
-
-        print(s, first_zeroes)
-
-        last_ones = 0
-
-        for i in range(len(s)-1, -1, -1):
-            if s[i] == '0':
-                break
-            last_ones += 1
-
-        s = s[0:len(s)-last_ones]
-
-        print(s)
-
-
-        zero_to_one = 0
-        one_to_zero = 0
-
-
-        for i in range(len(s)-1, -1, -1):
-
-            if s[i] == '0':
-                zero_to_one += 1
-                #  print('not ok')
-
-            if s[i] == '1':
-                one_to_zero += 1
-                #  print('ok')
-
-        #  print(one_to_zero, zero_to_one)
-        return min(one_to_zero, zero_to_one)
+            print(c, ans, num)
+        return ans
 
 if __name__ == "__main__":
     s = Solution()
-    #  print(s.minFlipsMonoIncr("00110"))
+    # print(s.minFlipsMonoIncr("00110"))
     #  print(s.minFlipsMonoIncr("010110"))
     #  print(s.minFlipsMonoIncr("00011000"))
     #  print(s.minFlipsMonoIncr("0101100011"))
