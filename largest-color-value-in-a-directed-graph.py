fb895eb7c997fa610279cb5ba2b7f8a0985f518a
desertsniper87 <torshobuet@gmail.com>
2023-04-09 22:28:32 +0600

    bal

---
+++ largest-color-value-in-a-directed-graph.py
@@ -0,0 +1,69 @@
+from collections import defaultdict
+
+class Solution:
+    def largestPathValue(self, colors: str, edges: List[List[int]]) -> int:
+        pass
+
+
+
+
+class Graph:
+	def __init__(self, vertices):
+		self.graph = defaultdict(list) # dictionary containing adjacency List
+		self.V = vertices # No. of vertices
+
+	# function to add an edge to graph
+	def addEdge(self, u, v):
+		self.graph[u].append(v)
+
+	# neighbors generator given key
+	def neighbor_gen(self, v):
+		for k in self.graph[v]:
+			yield k
+
+	# non recursive topological sort
+	def nonRecursiveTopologicalSortUtil(self, v, visited, stack):
+
+		# working stack contains key and the corresponding current generator
+		working_stack = [(v, self.neighbor_gen(v))]
+
+		while working_stack:
+			# get last element from stack
+			v, gen = working_stack.pop()
+			visited[v] = True
+
+			# run through neighbor generator until it's empty
+			for next_neighbor in gen:
+				if not visited[next_neighbor]: # not seen before?
+					# remember current work
+					working_stack.append((v, gen))
+					# restart with new neighbor
+					working_stack.append(
+						(next_neighbor, self.neighbor_gen(next_neighbor)))
+					break
+			else:
+				# no already-visited neighbor (or no more of them)
+				stack.append(v)
+
+	# The function to do Topological Sort.
+	def nonRecursiveTopologicalSort(self):
+		# Mark all the vertices as not visited
+		visited = [False]*self.V
+
+		# result stack
+		stack = []
+
+		# Call the helper function to store Topological
+		# Sort starting from all vertices one by one
+		for i in range(self.V):
+			if not(visited[i]):
+				self.nonRecursiveTopologicalSortUtil(i, visited, stack)
+		# Print contents of the stack in reverse
+		stack.reverse()
+		print(stack)
+
+
+# Driver code
+if __name__ == "__main__":
+
+
