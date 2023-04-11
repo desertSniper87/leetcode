03724de0e0aa4fb832400d49f605326790649922
desertsniper87 <torshobuet@gmail.com>
2023-04-11 23:26:00 +0600

    easypeasy

---
+++ largest-color-value-in-a-directed-graph.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def largestPathValue(self, colors: str, edges: List[List[int]]) -> int:
+        pass
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.largestPathValue("abaca", [[0,1],[0,2],[2,3],[3,4]]))
+    
+
