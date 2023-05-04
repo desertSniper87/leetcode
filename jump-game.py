67e43f85fc896ac34a76243847ec2831f4d228cb
desertsniper87 <torshobuet@gmail.com>
2023-05-05 00:12:55 +0600

    genious

--- jump-game.py
+++ jump-game.py
@@ -12,31 +12,30 @@ class Solution:
         elif l == 2 and  nums[0] == 0:
             return False
             
-        table[l - 1] = True
-        table[l - 2] = True
+        table[0] = True
 
         #  print(start)
-        stack = [l - 2]
+        stack = [0]
 
         while stack:
             start = stack.pop()
-            for i in range(nums[start]):
-                print(i)
-                index = start-i-1
-                print(index)
-                if not table[index] and index >= 0 and nums[index] + index >= start:
-                    print(start, index, nums[index])
+            if start + nums[start] >= l: 
+                return True
+            for i in range(nums[start] + start + 1, 0, -1):
+                index = start +  i
+                if index < l and not table[index] and index - start <= nums[start]:
                     table[index] = True
-                    stack.append(index)
+                    stack.insert(0, index)
+                    
 
 
         
-        return table[0]
+        return table[l-1]
 
 if __name__ == "__main__":
     s = Solution()
-    #  print(s.canJump([2,3,1,1,4]))
-    #  print(s.canJump([3,2,1,0,4]))
-    #  print(s.canJump([0,1]))
-    #  print(s.canJump([0,2,3]))
+    print(s.canJump([2,3,1,1,4]))
+    print(s.canJump([3,2,1,0,4]))
+    print(s.canJump([0,1]))
+    print(s.canJump([0,2,3]))
     print(s.canJump([2,0,0]))
