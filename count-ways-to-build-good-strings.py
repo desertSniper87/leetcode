1134ac4abd2c8c9b16622ea6dd4c437e1e6e0244
desertsniper87 <torshobuet@gmail.com>
2023-05-13 23:30:01 +0600

    God give me strength

--- count-ways-to-build-good-strings.py
+++ count-ways-to-build-good-strings.py
@@ -1,7 +1,30 @@
 class Solution:
     def countGoodStrings(self, low: int, high: int, zero: int, one: int) -> int:
-        pass
+        mod = 10**9 + 7
+        dp = {}
+
+        def dfs(i):
+            if i > high:
+                return 0
+
+            if i in dp:
+                return dp[i]
+
+            dp[i] = 1 if i >= low else 0
+            dp[i] += dfs(i + zero) + dfs (i + one)
+
+            return dp[i] % mod
+
+
+        return dfs(0)
+
+
 
 if __name__ == "__main__":
     s = Solution()
-    s.countGoodStrings(
+    #  low = 3; high = 3; zero = 1; one = 1
+    #  s.countGoodStrings(low, high, zero, one)
+    #  low = 3; high = 4; zero = 2; one = 2
+    #  print(s.countGoodStrings(low, high, zero, one))
+    low = 2; high = 3; zero = 1; one = 2
+    print(s.countGoodStrings(low, high, zero, one))
