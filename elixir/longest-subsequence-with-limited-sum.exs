85db795ecb9cef2f72d3ad5052384cf92787c8d9
desertsniper87 <torshobuet@gmail.com>
2024-07-13 16:25:50 +0600

    leet

--- elixir/longest-subsequence-with-limited-sum.exs
+++ elixir/longest-subsequence-with-limited-sum.exs
@@ -1,10 +1,28 @@
 defmodule Solution do
   @spec answer_queries(nums :: [integer], queries :: [integer]) :: [integer]
   def answer_queries(nums, queries) do
+    prefix_sum =
+      nums
+      |> Enum.sort()
+      |> Enum.scan(fn x, y -> x + y end)
+
+    queries
+    |> Enum.map(&get_longest_subseq(&1, prefix_sum))
+  end
+
+  defp get_longest_subseq(target, prefix_sum) do
+    prefix_sum
+    |> Enum.filter(fn x -> x <= target end)
+    |> length()
   end
 end
 
 nums = [4, 5, 2, 1]
 queries = [3, 10, 21]
 
-Solution.answer_queries(nums, queries)
+IO.inspect(Solution.answer_queries(nums, queries))
+
+nums = [2, 3, 4, 5]
+queries = [1]
+
+IO.inspect(Solution.answer_queries(nums, queries))
